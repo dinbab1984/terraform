9e9a8393ccd2ca8833f68e7bdb71a1fe067be7b9
dinbab1984@gmail.com <dinbab1984@gmail.com>
2024-10-06 23:15:04 +0200

    aws pl

--- aws-databricks-workspace-pl/pl-dat-res-vpc-ep.tf
+++ aws-databricks-workspace-pl/pl-dat-res-vpc-ep.tf
@@ -29,9 +29,57 @@ resource "aws_vpc_endpoint" "s3" {
   depends_on = [aws_vpc.this, aws_route_table.private_subnet_rt]
 }
 
+
+//security group for other aws resources
+// VPC's Default Security Group
+locals {
+  sg_egress_ports              = [443, 3306, 2443, 6666]
+  sg_ingress_protocol          = ["tcp", "udp"]
+  sg_egress_protocol           = ["tcp", "udp"]
+}
+
+resource "aws_security_group" "default_sg" {
+  name        = "${var.name_prefix}-spoke-vpc-sg"
+  description = "Default security group to allow inbound/outbound from the VPC"
+  vpc_id      = aws_vpc.this.id
+  depends_on  = [aws_vpc.this]
+  //Inbound internal traffic between resources in this security group (all ports)
+  dynamic "ingress" {
+    for_each = local.sg_ingress_protocol
+    content {
+      from_port = 0
+      to_port   = 65535
+      protocol  = ingress.value
+      self      = true
+    }
+  }
+  //Outbound internal traffic between resources in this security group (all ports)
+  dynamic "egress" {
+    for_each = local.sg_egress_protocol
+    content {
+      from_port = 0
+      to_port   = 65535
+      protocol  = egress.value
+      self      = true
+    }
+  }
+  //Outbound tcp access to 0.0.0.0/0 , ports 443, 3306, 6666
+  dynamic "egress" {
+    for_each = local.sg_egress_ports
+    content {
+      from_port   = egress.value
+      to_port     = egress.value
+      protocol    = "tcp"
+      cidr_blocks = ["0.0.0.0/0"]
+    }
+  }
+
+  tags = var.tags
+}
+
 resource "aws_vpc_endpoint" "kinesis-streams" {
   vpc_id              = aws_vpc.this.id
-  security_group_ids  = [aws_vpc.this.default_security_group_id]
+  security_group_ids  = [aws_security_group.default_sg.id]
   service_name        = "com.amazonaws.${var.aws_region}.kinesis-streams"
   vpc_endpoint_type   = "Interface"
   private_dns_enabled = true
@@ -47,7 +95,7 @@ module "vpc_endpoints" {
   version = "3.11.0"
 
   vpc_id = aws_vpc.this.id
-  security_group_ids = [aws_vpc.this.default_security_group_id]
+  security_group_ids = [aws_security_group.default_sg.id]
 
   endpoints = {
     sts = {
--- aws-databricks-workspace-pl/pl-dbx-res-configs-network.tf
+++ aws-databricks-workspace-pl/pl-dbx-res-configs-network.tf
@@ -3,7 +3,7 @@ resource "databricks_mws_networks" "this" {
   provider           = databricks.mws
   account_id         = var.databricks_account_id
   network_name       = "${var.name_prefix}-network-configuration"
-  security_group_ids = [aws_vpc.this.default_security_group_id]//[aws_security_group.default_sg.id]
+  security_group_ids = [aws_security_group.default_sg.id]
   subnet_ids         = [for k, v in var.private_subnets_cidr : aws_subnet.private_subnet[k].id]
   vpc_id             = aws_vpc.this.id
   vpc_endpoints {
