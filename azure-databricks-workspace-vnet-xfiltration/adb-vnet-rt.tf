5279535cf5e30365039b9ec36f184db58033aa0b
dinbab1984@gmail.com <dinbab1984@gmail.com>
2024-12-30 18:21:18 +0100

    exfiltration routes art and metastore

--- azure-databricks-workspace-vnet-xfiltration/adb-vnet-rt.tf
+++ azure-databricks-workspace-vnet-xfiltration/adb-vnet-rt.tf
@@ -1,3 +1,21 @@
+//get ip adddresses
+locals {
+  artefactstoreips = toset(flatten([for k, v in data.dns_a_record_set.artefact_store : v.addrs]))
+  metastoreips = toset(flatten([for k, v in data.dns_a_record_set.metastore : v.addrs]))
+}
+
+//get ip adddresses of artefactstorefqdns
+data "dns_a_record_set" "artefact_store" {
+  for_each = toset(var.artefactstorefqdns)
+  host     = each.value
+}
+
+//get ip adddresses of legacy metatorefqdns
+data "dns_a_record_set" "metastore" {
+  for_each = toset(var.metastorefqdns)
+  host     = each.value
+}
+
 resource "azurerm_route_table" "adbroute" {
   //route all traffic from spoke vnet to hub vnet
   name                = "spoke-routetable"
@@ -10,12 +28,31 @@ resource "azurerm_route_table" "adbroute" {
     next_hop_type          = "VirtualAppliance"
     next_hop_in_ip_address = azurerm_firewall.hubfw.ip_configuration.0.private_ip_address // extract single item
   }
-  route {
+  
+  /*route {
     name                   = "to-artefact"
     address_prefix         = var.storage_servicetag
     next_hop_type          = "Internet"
   }
-  depends_on = [ azurerm_resource_group.hubrg, azurerm_firewall.hubfw ]
+  */
+  dynamic "route" {
+    for_each = local.artefactstoreips
+    content {
+      name           = "to-artefact-${route.value}"
+      address_prefix = "${route.value}/32"
+      next_hop_type  = "Internet" // since Artefact Store is azure service IP, traffic will remain in azure backbone, not Internet
+    }
+  }
+  dynamic "route" {
+    for_each = local.metastoreips
+    content {
+      name           = "to-metastore-${route.value}"
+      address_prefix = "${route.value}/32"
+      next_hop_type  = "Internet" // since Metastore Store is azure service IP, traffic will remain in azure backbone, not Internet
+    }
+  }
+
+  depends_on = [ azurerm_resource_group.hubrg, azurerm_firewall.hubfw, data.dns_a_record_set.artefact_store , data.dns_a_record_set.metastore]
 }
 
 resource "azurerm_subnet_route_table_association" "vnetsubnetudr" {
