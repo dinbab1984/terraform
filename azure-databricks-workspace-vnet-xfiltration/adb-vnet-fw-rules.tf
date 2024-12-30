2b86cb5f8d723ecad1cdb61a5814e4d8970c7c9a
dinbab1984@gmail.com <dinbab1984@gmail.com>
2024-12-30 15:20:05 +0100

    exfiltration fw network rules telemetry

--- azure-databricks-workspace-vnet-xfiltration/adb-vnet-fw-rules.tf
+++ azure-databricks-workspace-vnet-xfiltration/adb-vnet-fw-rules.tf
@@ -5,6 +5,73 @@ resource "azurerm_public_ip" "fwpublicip" {
   resource_group_name = azurerm_resource_group.hubrg.name
   allocation_method   = "Static"
   sku                 = "Standard"
+  tags = var.tags
+  depends_on = [ azurerm_resource_group.hubrg ]
+}
+
+//firewall policy
+resource "azurerm_firewall_policy" "hubfw_policy" {
+  name                     = "hubfw-policy"
+  resource_group_name      = azurerm_resource_group.hubrg.name
+  location                 = azurerm_resource_group.hubrg.location
+  sku                      = "Standard"
+  threat_intelligence_mode = "Alert"
+  dns {
+    proxy_enabled = true
+  }
+  tags = var.tags
+  depends_on = [ azurerm_resource_group.hubrg ]
+}
+
+//Firewall rule - 2. Telemetry (Azure EventHub)
+resource "azurerm_firewall_policy_rule_collection_group" "net_policy_rule_collection_group" {
+  name               = "ADBtNetworkRuleCollectionGroup"
+  firewall_policy_id = azurerm_firewall_policy.hubfw_policy.id
+  priority           = 200
+  network_rule_collection {
+    name     = "adbtelemetryfqdn"
+    action   = "Allow"
+    priority = 200
+    rule {
+      name                  = "databricks-telemetry"
+      source_addresses      = [for k,v in data.azurerm_subnet.ws_subnets : data.azurerm_subnet.ws_subnets[k].address_prefixes[0]]
+      destination_fqdns     = var.telemetryfqdns
+      destination_ports     = ["9093"]
+      protocols             = ["TCP"]
+    }
+  }
+  depends_on = [ azurerm_firewall_policy.hubfw_policy , data.azurerm_subnet.ws_subnets ]
+}
+
+//Firewall rules - 1. Log Store (Azure Blob) & 3. System Table Store(Azure ADLS)
+resource "azurerm_firewall_policy_rule_collection_group" "app_policy_rule_collection_group" {
+  name               = "ADBApplicationtRuleCollectionGroup"
+  firewall_policy_id = azurerm_firewall_policy.hubfw_policy.id
+  priority           = 300
+  application_rule_collection {
+    name     = "ADBApplicationRuleCollection"
+    action   = "Allow"
+    priority = 500
+    rule {
+      name = "databricks-log-store"
+      source_addresses = [for k,v in data.azurerm_subnet.ws_subnets : data.azurerm_subnet.ws_subnets[k].address_prefixes[0]]
+      destination_fqdns = var.logstorefqdns
+      protocols {
+        port = "443"
+        type = "Https"
+      }
+    }
+    rule {
+      name = "databricks-system-table-store"
+      source_addresses = [for k,v in data.azurerm_subnet.ws_subnets : data.azurerm_subnet.ws_subnets[k].address_prefixes[0]]
+      destination_fqdns = var.systemtablestorefqdns
+      protocols {
+        port = "443"
+        type = "Https"
+      }
+    }
+  }
+  depends_on = [ azurerm_firewall_policy.hubfw_policy,data.azurerm_subnet.ws_subnets ]
 }
 
 //Firewall resource
@@ -12,19 +79,21 @@ resource "azurerm_firewall" "hubfw" {
   name                = "hubfirewall"
   location            = azurerm_resource_group.hubrg.location
   resource_group_name = azurerm_resource_group.hubrg.name
-  dns_proxy_enabled   = true //for firewall network rule with fqdns destination
+  //dns_proxy_enabled   = true //for firewall network rule with fqdns destination
   sku_name            = "AZFW_VNet"
   sku_tier            = "Standard"
 
   ip_configuration {
-    name                 = "configuration"
+    name                 = "hubfw-configuration"
     subnet_id            = azurerm_subnet.hubfw.id
     public_ip_address_id = azurerm_public_ip.fwpublicip.id
   }
-  depends_on = [ azurerm_public_ip.fwpublicip ]
+  firewall_policy_id = azurerm_firewall_policy.hubfw_policy.id
+  tags = var.tags
+  depends_on = [ azurerm_public_ip.fwpublicip, azurerm_firewall_policy.hubfw_policy ]
 }
 
-
+/*
 //Firewall rules - 1. Log Store (Azure Blob)
 resource "azurerm_firewall_application_rule_collection" "adblogstorefqdn" {
   name                = "adblogstorefqdn"
@@ -95,6 +164,8 @@ resource "azurerm_firewall_application_rule_collection" "adbsystemstablestorefqd
   }
   depends_on = [ azurerm_firewall.hubfw, data.azurerm_subnet.ws_subnets ]
 }
+
+*/
 
 /*
 
