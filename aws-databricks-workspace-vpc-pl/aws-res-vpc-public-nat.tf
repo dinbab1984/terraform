4e90f03032c5074cd8ec25db650a9d122b9f269c
dinbab1984@gmail.com <dinbab1984@gmail.com>
2025-01-06 19:07:59 +0100

    metastore ip for nat igw route

--- aws-databricks-workspace-vpc-pl/aws-res-vpc-public-nat.tf
+++ aws-databricks-workspace-vpc-pl/aws-res-vpc-public-nat.tf
@@ -50,31 +50,46 @@ resource "aws_route_table_association" "nat_rt_association" {
 }
 
 /* Adding routes to route tables */
-
 //get ip adddress of metastore
 data "dns_a_record_set" "metastore" {
   host = var.metastorefqdn
 }
 
-output "metastoreip" {
-  value = data.dns_a_record_set.metastore.addrs[0]
-  
+locals {
+   private_subnet_rt_metastoreip = flatten([
+    for addrs in data.dns_a_record_set.metastore.addrs : [
+      for region_az, cidr in var.private_subnets_cidr : {
+        metastoreip = addrs
+        region_az   = region_az
+      }
+    ]
+  ])
 }
 
+/*
+output "private_subnet_rt_metastoreip" {
+  value = local.private_subnet_rt_metastoreip
+  
+}
+*/
+
 resource "aws_route" "private_to_public_nat_gtw" {
-  for_each               = var.private_subnets_cidr
-  route_table_id         = aws_route_table.private_subnet_rt[each.key].id
-  destination_cidr_block = "${data.dns_a_record_set.metastore.addrs[0]}/32"
-  //destination_cidr_block = "3.255.48.43/32" //"0.0.0.0/0"
-  nat_gateway_id         = aws_nat_gateway.nat_gw[each.key].id
+  for_each               = { for k, v in local.private_subnet_rt_metastoreip : k => v }
+  route_table_id         =  aws_route_table.private_subnet_rt[each.value.region_az].id
+  destination_cidr_block = "${each.value.metastoreip}/32"
+  //destination_cidr_block = "3.255.48.43/32"
+  //destination_cidr_block = "0.0.0.0/0"
+  nat_gateway_id         = aws_nat_gateway.nat_gw[each.value.region_az].id
   depends_on             = [aws_route_table.private_subnet_rt, aws_nat_gateway.nat_gw]
 }
 
+
 resource "aws_route" "public_nat_gtw_to_igw" {
-  for_each               = var.nat_subnets_cidr
-  route_table_id         = aws_route_table.nat_subnet_rt[each.key].id
-  destination_cidr_block = "${data.dns_a_record_set.metastore.addrs[0]}/32"
-  //destination_cidr_block = "3.255.48.43/32" //"0.0.0.0/0"
+  for_each               = { for k, v in local.private_subnet_rt_metastoreip : k => v }
+  route_table_id         =  aws_route_table.nat_subnet_rt[each.value.region_az].id
+  destination_cidr_block = "${each.value.metastoreip}/32"
+  //destination_cidr_block = "3.255.48.43/32" 
+  //destination_cidr_block = "0.0.0.0/0"
   gateway_id             = aws_internet_gateway.igw.id
   depends_on             = [aws_route_table.nat_subnet_rt, aws_internet_gateway.igw]
 }
